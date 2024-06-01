34ee1af580cacc7105b23354d0d074a21422a09c
Sid20rathi <sid20rathi@gmail.com>
2024-06-01 12:04:36 +0530

    inital

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    "Marketing is no longer about the stuff that you make , but the stories you tell"
+                </div>
+                <div className="max-w-md text-xl font-semibold text-left mt-4">
+                   The Latest Buzz
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                   Inspired for Impact
+                </div>
+            </div>
+        </div>
+        
+    </div>
+}
